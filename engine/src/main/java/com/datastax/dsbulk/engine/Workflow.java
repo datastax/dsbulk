c6f955dc88beaf34888414ec968bd7dcd9971f13
Alexandre Dutra <alexandre.dutra@datastax.com>
2018-01-06 11:26:30 +0100

    Address feedback

--- engine/src/main/java/com/datastax/dsbulk/engine/Workflow.java
+++ engine/src/main/java/com/datastax/dsbulk/engine/Workflow.java
@@ -39,14 +39,14 @@ public interface Workflow extends AutoCloseable {
   boolean execute() throws Exception;
 
   /**
-   * CLoses the workflow and releases all open resources (file descriptors, socket connections,
+   * Closes the workflow and releases all open resources (file descriptors, socket connections,
    * etc.).
    *
    * <p>This method is guaranteed to be called once and only once by the execution engine, and only
    * after the workflow has finished its {@link #execute() execution}, regardless of whether the
    * execution finished normally or exceptionally.
    *
-   * @throws Exception if the engine workflow an error while closing open resources.
+   * @throws Exception if the workflow encountered an error while closing open resources.
    */
   @Override
   void close() throws Exception;
