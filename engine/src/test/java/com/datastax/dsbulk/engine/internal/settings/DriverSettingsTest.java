983f77858f883e419c8fcb02e3ecf55a6b2d846f
Andrew Tolbert <andrew.tolbert@datastax.com>
2018-02-06 05:42:26 -0600

    DAT-225: Change driver.pooling.local.connections default to 8 (#189)

--- engine/src/test/java/com/datastax/dsbulk/engine/internal/settings/DriverSettingsTest.java
+++ engine/src/test/java/com/datastax/dsbulk/engine/internal/settings/DriverSettingsTest.java
@@ -104,8 +104,8 @@ class DriverSettingsTest {
     assertThat(queryOptions.getFetchSize()).isEqualTo(5000);
     assertThat(queryOptions.getDefaultIdempotence()).isTrue();
     PoolingOptions poolingOptions = configuration.getPoolingOptions();
-    assertThat(poolingOptions.getCoreConnectionsPerHost(LOCAL)).isEqualTo(4);
-    assertThat(poolingOptions.getMaxConnectionsPerHost(LOCAL)).isEqualTo(4);
+    assertThat(poolingOptions.getCoreConnectionsPerHost(LOCAL)).isEqualTo(8);
+    assertThat(poolingOptions.getMaxConnectionsPerHost(LOCAL)).isEqualTo(8);
     assertThat(poolingOptions.getCoreConnectionsPerHost(REMOTE)).isEqualTo(1);
     assertThat(poolingOptions.getMaxConnectionsPerHost(REMOTE)).isEqualTo(1);
     assertThat(poolingOptions.getMaxRequestsPerConnection(LOCAL)).isEqualTo(32768);
