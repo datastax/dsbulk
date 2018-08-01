4078bb9b7ce32a9b2531ff07e8b9bfe79c26597f
Alexandre Dutra <alexdut@gmail.com>
2018-08-01 13:08:09 +0200

    Log error and warn messages with bold font instead of bright

--- engine/src/main/java/com/datastax/dsbulk/engine/internal/log/HighlightingMessageConverter.java
+++ engine/src/main/java/com/datastax/dsbulk/engine/internal/log/HighlightingMessageConverter.java
@@ -14,17 +14,20 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.pattern.MessageConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
-/** A {@link MessageConverter} that highlights WARN and ERROR messages in red. */
+/**
+ * A {@link MessageConverter} that highlights WARN and ERROR messages in bold yellow and bold red
+ * respectively.
+ */
 public class HighlightingMessageConverter extends MessageConverter {
 
   @Override
   public String convert(ILoggingEvent event) {
     String msg = super.convert(event);
     if (event.getLevel().toInt() == Level.WARN_INT) {
-      msg = ansi().fgYellow().a(event.getFormattedMessage()).reset().toString();
+      msg = ansi().bold().fgYellow().a(event.getFormattedMessage()).reset().toString();
     }
     if (event.getLevel().toInt() >= Level.ERROR_INT) {
-      msg = ansi().fgBrightRed().a(event.getFormattedMessage()).reset().toString();
+      msg = ansi().bold().fgRed().a(event.getFormattedMessage()).reset().toString();
     }
     return msg;
   }
