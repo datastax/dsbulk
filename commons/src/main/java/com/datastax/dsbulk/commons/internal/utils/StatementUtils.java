501870675757d91cd1acba9d31850902b1140cd1
tomekl007 <tomasz.lelek@datastax.com>
2018-12-05 15:13:17 +0100

    DAT-352 introduce StatementUtils - test for Bounded and Regular Statement

---
+++ commons/src/main/java/com/datastax/dsbulk/commons/internal/utils/StatementUtils.java
@@ -0,0 +1,62 @@
+package com.datastax.dsbulk.commons.internal.utils;
+
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.CodecRegistry;
+import com.datastax.driver.core.DriverCoreHooks;
+import com.datastax.driver.core.ProtocolVersion;
+import com.datastax.driver.core.RegularStatement;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.StatementWrapper;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+public class StatementUtils {
+  public static long getDataSize(Statement stmt, ProtocolVersion version, CodecRegistry registry) {
+    long dataSize = 0;
+    if (stmt instanceof BoundStatement) {
+      BoundStatement bs = (BoundStatement) stmt;
+      int numVariables = bs.preparedStatement().getVariables().size();
+      for (int i = 0; i < numVariables; i++) {
+        ByteBuffer bb = bs.getBytesUnsafe(i);
+        dataSize += bb == null ? 0 : bb.remaining();
+      }
+    } else if (stmt instanceof RegularStatement) {
+      RegularStatement rs = (RegularStatement) stmt;
+      if (rs.hasValues()) {
+        if (rs.usesNamedValues()) {
+          Map<String, ByteBuffer> values = rs.getNamedValues(version, registry);
+          if (values != null) {
+            for (ByteBuffer bb : values.values()) {
+              dataSize += bb == null ? 0 : bb.remaining();
+            }
+          }
+        } else {
+          ByteBuffer[] values = rs.getValues(version, registry);
+          if (values != null) {
+            for (ByteBuffer bb : values) {
+              dataSize += bb == null ? 0 : bb.remaining();
+            }
+          }
+        }
+      }
+    } else if (stmt instanceof BatchStatement) {
+      BatchStatement bs = (BatchStatement) stmt;
+
+      for (Statement st : bs.getStatements()) {
+        dataSize += getDataSize(st, version, registry);
+      }
+
+    } else if (stmt instanceof StatementWrapper) {
+      StatementWrapper sw = (StatementWrapper) stmt;
+      dataSize += getDataSize(
+          DriverCoreHooks.wrappedStatement(sw),
+          version,
+          registry
+      );
+
+    }
+    return dataSize;
+  }
+}
---
+++ commons/src/test/java/com/datastax/dsbulk/commons/internal/utils/StatementUtilsTest.java
@@ -0,0 +1,114 @@
+package com.datastax.dsbulk.commons.internal.utils;
+
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.CodecRegistry;
+import com.datastax.driver.core.ColumnDefinitions;
+import com.datastax.driver.core.DataType;
+import com.datastax.driver.core.DriverCoreCommonsTestHooks;
+import com.datastax.driver.core.PreparedId;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ProtocolVersion;
+import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.utils.Bytes;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.nio.ByteBuffer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class StatementUtilsTest {
+  private static final byte[] MOCK_PAGING_STATE = Bytes.getArray(Bytes.fromHexString("0xdeadbeef"));
+  private static final ByteBuffer MOCK_PAYLOAD_VALUE1 = Bytes.fromHexString("0xabcd");
+  private static final ByteBuffer MOCK_PAYLOAD_VALUE2 = Bytes.fromHexString("0xef");
+  private static final ImmutableMap<String, ByteBuffer> MOCK_PAYLOAD =
+      ImmutableMap.of("key1", MOCK_PAYLOAD_VALUE1, "key2", MOCK_PAYLOAD_VALUE2);
+
+  @Mock
+  private PreparedStatement preparedStatement;
+
+  @BeforeAll
+  void setup() {
+    MockitoAnnotations.initMocks(this);
+
+    ColumnDefinitions columnDefinitions =
+        DriverCoreCommonsTestHooks.newColumnDefinitions(
+            DriverCoreCommonsTestHooks.newDefinition("ks", "table", "c1", DataType.cint()),
+            DriverCoreCommonsTestHooks.newDefinition("ks", "table", "c2", DataType.text())
+        );
+
+    PreparedId preparedId = DriverCoreCommonsTestHooks.newPreparedId(
+        columnDefinitions,new int[0], ProtocolVersion.DSE_V2
+    );
+    Mockito.when(preparedStatement.getPreparedId()).thenReturn(preparedId);
+
+
+    Mockito.when(preparedStatement.getVariables()).thenReturn(columnDefinitions);
+    Mockito.when(preparedStatement.getIncomingPayload()).thenReturn(null);
+    Mockito.when(preparedStatement.getOutgoingPayload()).thenReturn(null);
+    Mockito.when(preparedStatement.getCodecRegistry()).thenReturn(CodecRegistry.DEFAULT_INSTANCE);
+  }
+
+  @Test
+  void should_measure_size_of_simple_statement() {
+    String queryString = "SELECT release_version FROM system.local WHERE key = ?";
+    SimpleStatement statement = new SimpleStatement(queryString);
+    int expectedSize = 0;
+    assertThat(getDataSize(statement)).isEqualTo(expectedSize);
+
+    SimpleStatement statementWithAnonymousValue =
+        new SimpleStatement(statement.getQueryString(), "local");
+    assertThat(getDataSize(statementWithAnonymousValue))
+        .isEqualTo(
+            expectedSize + "local".getBytes(Charsets.UTF_8).length // value
+        );
+
+    SimpleStatement statementWithNamedValue =
+        new SimpleStatement(
+            statement.getQueryString(), ImmutableMap.<String, Object>of("key", "local")); //key not taken into account
+
+    assertThat(getDataSize(statementWithNamedValue))
+        .isEqualTo(
+            expectedSize + "local".getBytes(Charsets.UTF_8).length // value
+        );
+
+
+    statement.setOutgoingPayload(MOCK_PAYLOAD);
+    assertThat(getDataSize(statement)).isEqualTo(expectedSize); //payload not taken into account
+  }
+
+  @Test
+  void should_measure_size_of_bound_statement() {
+    BoundStatement statement = new BoundStatement(preparedStatement);
+    int expectedSize = 0;
+    assertThat(getDataSize(statement)).isEqualTo(expectedSize);
+
+    statement.setInt(0, 0);
+    expectedSize += 4; // serialized value (we already have its size from when it was null above)
+    statement.setString(1, "test");
+    expectedSize += "test".getBytes(Charsets.UTF_8).length;
+    assertThat(getDataSize(statement)).isEqualTo(expectedSize);
+
+    statement.setPagingStateUnsafe(MOCK_PAGING_STATE);//not taken into account
+    assertThat(getDataSize(statement)).isEqualTo(expectedSize);
+
+    statement.setOutgoingPayload(MOCK_PAYLOAD);//not taken into account
+    assertThat(getDataSize(statement)).isEqualTo(expectedSize);
+  }
+
+
+
+  private long getDataSize(Statement statement) {
+    return StatementUtils.getDataSize(statement, ProtocolVersion.DSE_V2, CodecRegistry.DEFAULT_INSTANCE);
+  }
+
+
+}
