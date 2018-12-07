ad6c318e255a6f0cb1fa631db075c8284a8e9a71
Alexandre Dutra <alexandre.dutra@datastax.com>
2018-12-07 12:02:30 +0100

    Add javadocs

--- commons/src/main/java/com/datastax/dsbulk/commons/internal/utils/StatementUtils.java
+++ commons/src/main/java/com/datastax/dsbulk/commons/internal/utils/StatementUtils.java
@@ -18,9 +18,34 @@ import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.StatementWrapper;
 import java.nio.ByteBuffer;
 import java.util.Map;
+import org.jetbrains.annotations.NotNull;
 
 public class StatementUtils {
-  public static long getDataSize(Statement stmt, ProtocolVersion version, CodecRegistry registry) {
+
+  /**
+   * Evaluates the data size contained in the given statement.
+   *
+   * <p>This only applies to mutations (INSERT, UPDATE, DELETE statements), and is specially useful
+   * for BATCH statements, where this method can be used to guess if a given batch risks to exceed
+   * the thresholds defined server-side in the in cassandra.yaml configuration file for the options
+   * {@code batch_size_warn_threshold_in_kb} and {@code batch_size_fail_threshold_in_kb}.
+   *
+   * <p>The actual algorithm used by Apache Cassandra can be found in {@code
+   * org.apache.cassandra.db.IMutation.dataSize()} but cannot be easily reproduced client-side.
+   * Instead, this method tries to guess the actual amount, in bytes, that the mutations triggered
+   * by the statement will generate, but unfortunately the heuristic used here is not 100% accurate
+   * and sometimes underestimates or overestimates the actual data size.
+   *
+   * @see <a
+   *     href=https://docs.datastax.com/en/dse/6.0/dse-dev/datastax_enterprise/config/configCassandra_yaml.html#configCassandra_yaml__advProps">cassandra.yaml
+   *     configuration file</a>
+   * @param stmt The statement to inspect; cannot be {@code null}.
+   * @param version The protocol version to use; cannot be {@code null}.
+   * @param registry The codec registry to use; cannot be {@code null}.
+   * @return The data size contained in the statement.
+   */
+  public static long getDataSize(
+      @NotNull Statement stmt, @NotNull ProtocolVersion version, @NotNull CodecRegistry registry) {
     long dataSize = 0;
     if (stmt instanceof BoundStatement) {
       BoundStatement bs = (BoundStatement) stmt;
