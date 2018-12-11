226d41918a8f93fd50893fbe87b89635bcf248ce
Greg Bestland <Greg.Bestland@datastax.com>
2018-12-10 18:40:53 -0600

    Fix import formatting

--- engine/src/test/java/com/datastax/dsbulk/engine/tests/utils/JsonUtils.java
+++ engine/src/test/java/com/datastax/dsbulk/engine/tests/utils/JsonUtils.java
@@ -11,11 +11,10 @@ package com.datastax.dsbulk.engine.tests.utils;
 import java.net.URL;
 
 public class JsonUtils {
+
   public static final URL JSON_RECORDS = ClassLoader.getSystemResource("ip-by-country-sample.json");
   public static final URL JSON_RECORDS_UNIQUE =
       ClassLoader.getSystemResource("ip-by-country-unique.json");
-  public static final URL JSON_RECORDS_UNIQUE_MISSING =
-      ClassLoader.getSystemResource("ip-by-country-unique-missing.json");
   public static final URL JSON_RECORDS_CRLF =
       ClassLoader.getSystemResource("ip-by-country-crlf.json");
   public static final URL JSON_RECORDS_PARTIAL_BAD =
