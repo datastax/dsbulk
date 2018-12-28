634bb5f2a2dd3961d4478a3ea058734efb19eff3
tomekl007 <tomasz.lelek@datastax.com>
2018-12-28 13:03:54 +0100

    DAT-371 test for composite type

---
+++ engine/src/it/java/com/datastax/dsbulk/engine/ccm/CustomTypeEndToEndCCMIT.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * This software is subject to the below license agreement.
+ * DataStax may make changes to the agreement from time to time,
+ * and will post the amended terms at
+ * https://www.datastax.com/terms/datastax-dse-bulk-utility-license-terms.
+ */
+package com.datastax.dsbulk.engine.ccm;
+
+import com.datastax.driver.core.Session;
+import com.datastax.dsbulk.commons.tests.ccm.CCMCluster;
+import com.datastax.dsbulk.commons.tests.ccm.annotations.CCMConfig;
+import com.datastax.dsbulk.commons.tests.ccm.annotations.CCMRequirements;
+import com.datastax.dsbulk.commons.tests.logging.LogCapture;
+import com.datastax.dsbulk.commons.tests.logging.LogInterceptingExtension;
+import com.datastax.dsbulk.commons.tests.logging.LogInterceptor;
+import com.datastax.dsbulk.commons.tests.logging.StreamCapture;
+import com.datastax.dsbulk.commons.tests.logging.StreamInterceptingExtension;
+import com.datastax.dsbulk.commons.tests.logging.StreamInterceptor;
+import com.datastax.dsbulk.engine.DataStaxBulkLoader;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.datastax.dsbulk.commons.tests.assertions.CommonsAssertions.assertThat;
+import static com.datastax.dsbulk.commons.tests.ccm.CCMCluster.Type.DDAC;
+import static com.datastax.dsbulk.commons.tests.ccm.CCMCluster.Type.DSE;
+import static com.datastax.dsbulk.commons.tests.logging.StreamType.STDERR;
+import static com.datastax.dsbulk.commons.tests.utils.FileUtils.deleteDirectory;
+import static com.datastax.dsbulk.commons.tests.utils.StringUtils.quoteJson;
+import static java.nio.file.Files.createTempDirectory;
+
+@ExtendWith(LogInterceptingExtension.class)
+@ExtendWith(StreamInterceptingExtension.class)
+@CCMConfig(numberOfNodes = 1)
+@Tag("medium")
+@CCMRequirements(compatibleTypes = {DSE, DDAC})
+class CustomTypeEndToEndCCMIT extends EndToEndCCMITBase {
+
+  private final LogInterceptor logs;
+  private final StreamInterceptor stderr;
+  private Path logDir;
+  private Path unloadDir;
+  private static final URL CUSTOM_TYPES_CSV = ClassLoader.getSystemResource("custom-type.csv");
+
+  CustomTypeEndToEndCCMIT(
+      CCMCluster ccm,
+      Session session,
+      @LogCapture LogInterceptor logs,
+      @StreamCapture(STDERR) StreamInterceptor stderr) {
+    super(ccm, session);
+    this.logs = logs;
+    this.stderr = stderr;
+  }
+
+  @BeforeAll
+  void createTables() {
+    createTableWithCustomType(session);
+  }
+
+  private void createTableWithCustomType(Session session) {
+    session.execute("CREATE TABLE custom_types_table (k int PRIMARY KEY, c1 'DynamicCompositeType(s => UTF8Type, i => Int32Type)')");
+  }
+
+  @BeforeEach
+  void setUpDirs() throws IOException {
+    logDir = createTempDirectory("logs");
+    unloadDir = createTempDirectory("unload");
+  }
+
+  @BeforeEach
+  void truncateTable() {
+    session.execute("TRUNCATE custom_types_table");
+  }
+
+  @AfterEach
+  void deleteDirs() {
+    deleteDirectory(logDir);
+    deleteDirectory(unloadDir);
+  }
+
+  @BeforeEach
+  void clearLogs() {
+    logs.clear();
+    stderr.clear();
+  }
+
+  /** Simple test case which attempts to load and unload data using ccm. */
+  @Test
+  void full_load_unload() throws Exception {
+
+    List<String> args = new ArrayList<>();
+    args.add("load");
+    args.add("--log.directory");
+    args.add(quoteJson(logDir));
+    args.add("--connector.csv.url");
+    args.add(quoteJson(CUSTOM_TYPES_CSV));
+    args.add("--connector.csv.header");
+    args.add("false");
+    args.add("--schema.keyspace");
+    args.add(session.getLoggedKeyspace());
+    args.add("--schema.table");
+    args.add("custom_types_table");
+    args.add("--schema.mapping");
+    args.add("0=k, 1=c1");
+
+    int status = new DataStaxBulkLoader(addContactPointAndPort(args)).run();
+    assertThat(status).isZero();
+    validateResultSetSize(1, "SELECT * FROM custom_types_table");
+//    deleteDirectory(logDir);
+//
+//    args = new ArrayList<>();
+//    args.add("unload");
+//    args.add("--log.directory");
+//    args.add(quoteJson(logDir));
+//    args.add("--connector.csv.url");
+//    args.add(quoteJson(unloadDir));
+//    args.add("--connector.csv.header");
+//    args.add("false");
+//    args.add("--connector.csv.maxConcurrentFiles");
+//    args.add("1");
+//    args.add("--schema.keyspace");
+//    args.add(session.getLoggedKeyspace());
+//    args.add("--schema.table");
+//    args.add("ip_by_country");
+//    args.add("--schema.mapping");
+//    args.add(IP_BY_COUNTRY_MAPPING_INDEXED);
+//
+//    status = new DataStaxBulkLoader(addContactPointAndPort(args)).run();
+//    assertThat(status).isZero();
+//    validateOutputFiles(24, unloadDir);
+  }
+}
