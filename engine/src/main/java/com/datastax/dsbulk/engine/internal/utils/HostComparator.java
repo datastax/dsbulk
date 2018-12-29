08fb3631bbcc8db62d3528ae5c87ced452b690ca
Alexandre Dutra <alexandre.dutra@datastax.com>
2018-12-28 22:59:25 -0200

    Simplify HostComparator since Host.getSocketAddress() cannot return null

--- engine/src/main/java/com/datastax/dsbulk/engine/internal/utils/HostComparator.java
+++ engine/src/main/java/com/datastax/dsbulk/engine/internal/utils/HostComparator.java
@@ -14,9 +14,7 @@ import java.util.Comparator;
 public class HostComparator implements Comparator<Host> {
   @Override
   public int compare(Host o1, Host o2) {
-    if (o1 == o2) {
-      return 0;
-    } else if (o1.getSocketAddress() == null || o2.getSocketAddress() == null) {
+    if (o1.equals(o2)) {
       return 0;
     } else if (o1.getSocketAddress().getHostName() == null
         || o2.getSocketAddress().getHostName() == null) {
