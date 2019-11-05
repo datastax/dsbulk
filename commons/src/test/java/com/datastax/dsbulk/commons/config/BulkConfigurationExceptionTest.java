549740360a5fa64b3f53a324ffc6bc1aa46c7ba0
Alexandre Dutra <alexandre.dutra@datastax.com>
2019-11-05 14:25:55 +0100

    Add missing headers

--- commons/src/test/java/com/datastax/dsbulk/commons/config/BulkConfigurationExceptionTest.java
+++ commons/src/test/java/com/datastax/dsbulk/commons/config/BulkConfigurationExceptionTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * This software is subject to the below license agreement.
+ * DataStax may make changes to the agreement from time to time,
+ * and will post the amended terms at
+ * https://www.datastax.com/terms/datastax-dse-bulk-utility-license-terms.
+ */
 package com.datastax.dsbulk.commons.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
