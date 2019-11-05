127d5bc7a985d98b36d4604d2f948e5b1a815e23
Alexandre Dutra <alexandre.dutra@datastax.com>
2019-11-05 19:37:56 +0100

    Include common driver settings in generated application.conf template

--- engine/src/main/java/com/datastax/dsbulk/engine/internal/docs/ConfigurationFileCreator.java
+++ engine/src/main/java/com/datastax/dsbulk/engine/internal/docs/ConfigurationFileCreator.java
@@ -27,6 +27,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Map.Entry;
 import org.apache.commons.text.WordUtils;
 
 public class ConfigurationFileCreator {
@@ -34,6 +35,9 @@ public class ConfigurationFileCreator {
   private static final int LINE_LENGTH = 100;
   private static final int INDENT_LENGTH = 4;
   private static final String LINE_INDENT = StringUtils.nCopies(" ", INDENT_LENGTH);
+  private static final String INDENTED_ROW_OF_HASHES =
+      LINE_INDENT + StringUtils.nCopies("#", LINE_LENGTH - INDENT_LENGTH);
+  private static final String ROW_OF_HASHES = StringUtils.nCopies("#", LINE_LENGTH);
 
   public static void main(String[] args) throws IOException {
     try {
@@ -47,73 +51,9 @@ public class ConfigurationFileCreator {
           new PrintWriter(
               Files.newBufferedWriter(
                   dest, StandardCharsets.UTF_8, WRITE, CREATE, TRUNCATE_EXISTING));
-
-      String rowOfHashes = StringUtils.nCopies("#", LINE_LENGTH);
-      String indentedRowOfHashes =
-          LINE_INDENT + StringUtils.nCopies("#", LINE_LENGTH - INDENT_LENGTH);
-      pw.println(rowOfHashes);
-      pw.println("# This is a template configuration file for the DataStax Bulk Loader (DSBulk).");
-      pw.println("#");
-      pw.println("# This file is written in HOCON format; see");
-      pw.println("# https://github.com/typesafehub/config/blob/master/HOCON.md");
-      pw.println("# for more information on its syntax.");
-      pw.println("#");
-      pw.println(
-          wrapLines(
-              "# Uncomment settings as needed to configure "
-                  + "DSBulk. When this file is named application.conf and placed in the "
-                  + "/conf directory, it will be automatically picked up and used by default. "
-                  + "To use other file names see the -f command-line option."));
-      pw.println(rowOfHashes);
-      pw.println("");
-      pw.println("dsbulk {");
-      pw.println("");
-
-      Config referenceConfig = LoaderConfigFactory.createReferenceConfig();
-      Map<String, SettingsGroup> groups = SettingsGroupFactory.createGroups(referenceConfig);
-
-      for (Map.Entry<String, SettingsGroup> groupEntry : groups.entrySet()) {
-        String section = groupEntry.getKey();
-        if (section.equals("Common")) {
-          // In this context, we don't care about the "Common" pseudo-section.
-          continue;
-        }
-        pw.println(indentedRowOfHashes);
-        referenceConfig.getConfig(section).root().origin().comments().stream()
-            .filter(line -> !ConfigUtils.isTypeHint(line))
-            .filter(line -> !ConfigUtils.isLeaf(line))
-            .forEach(
-                l -> {
-                  pw.print(LINE_INDENT + "# ");
-                  pw.println(wrapIndentedLines(l));
-                });
-        pw.println(indentedRowOfHashes);
-
-        for (String settingName : groupEntry.getValue().getSettings()) {
-          ConfigValue value = ConfigUtils.getNullSafeValue(referenceConfig, settingName);
-
-          pw.println();
-          value.origin().comments().stream()
-              .filter(line -> !ConfigUtils.isTypeHint(line))
-              .filter(line -> !ConfigUtils.isLeaf(line))
-              .forEach(
-                  l -> {
-                    pw.print(LINE_INDENT + "# ");
-                    pw.println(wrapIndentedLines(l));
-                  });
-          pw.print(LINE_INDENT + "# Type: ");
-          pw.println(ConfigUtils.getTypeString(referenceConfig, settingName).orElse("arg"));
-          pw.print(LINE_INDENT + "# Default value: ");
-          pw.println(value.render(ConfigRenderOptions.concise()));
-          pw.print(LINE_INDENT);
-          pw.print("#");
-          pw.print(settingName.substring("dsbulk.".length()));
-          pw.print(" = ");
-          pw.println(value.render(ConfigRenderOptions.concise()));
-        }
-        pw.println();
-      }
-      pw.println("}");
+      printHeader(pw);
+      printDriverConfiguration(pw);
+      printDSBulkConfiguration(pw);
       pw.flush();
     } catch (Exception e) {
       System.err.println("Error encountered generating merged configuration file");
@@ -122,13 +62,153 @@ public class ConfigurationFileCreator {
     }
   }
 
+  private static void printHeader(PrintWriter pw) {
+    pw.println(ROW_OF_HASHES);
+    pw.println("# This is a template configuration file for the DataStax Bulk Loader (DSBulk)");
+    pw.println("# and the DataStax Java driver.");
+    pw.println("#");
+    pw.println("# This file is written in HOCON format; see");
+    pw.println("# https://github.com/typesafehub/config/blob/master/HOCON.md");
+    pw.println("# for more information on its syntax.");
+    pw.println("#");
+    pw.println(
+        wrapLines(
+            "# Uncomment settings as needed to configure "
+                + "DSBulk and/or the driver. When this file is named application.conf and placed "
+                + "in the /conf directory, it will be automatically picked up and used by default. "
+                + "To use other file names see the -f command-line option."));
+    pw.println(ROW_OF_HASHES);
+    pw.println("");
+  }
+
+  private static void printDriverConfiguration(PrintWriter pw) {
+    pw.println("# Java Driver configuration.");
+    pw.println(
+        "# The settings below are just a subset of all the configurable options of the driver. ");
+    pw.println(
+        "# See the Java Driver configuration reference for instructions on how to configure the driver properly:");
+    pw.println(
+        "# https://docs.datastax.com/en/developer/java-driver/4.3/manual/core/configuration/reference");
+    pw.println("datastax-java-driver {");
+    pw.println("");
+    pw.println(LINE_INDENT + "basic {");
+    pw.println("");
+    Config driverConfig =
+        LoaderConfigFactory.standaloneDSBulkReference().getConfig("datastax-java-driver.basic");
+    for (Entry<String, ConfigValue> groupEntry : driverConfig.entrySet()) {
+      String settingName = groupEntry.getKey();
+      ConfigValue value = ConfigUtils.getNullSafeValue(driverConfig, settingName);
+      value.origin().comments().stream()
+          .filter(line -> !ConfigUtils.isTypeHint(line))
+          .filter(line -> !ConfigUtils.isLeaf(line))
+          .forEach(
+              l -> {
+                pw.print(LINE_INDENT + LINE_INDENT + "# ");
+                pw.println(wrapIndentedLines(l, 2));
+              });
+      pw.print(LINE_INDENT + LINE_INDENT + "# Type: ");
+      pw.println(ConfigUtils.getTypeString(driverConfig, settingName).orElse("arg"));
+      pw.print(LINE_INDENT + LINE_INDENT + "# Default value: ");
+      pw.println(value.render(ConfigRenderOptions.concise()));
+      pw.print(LINE_INDENT + LINE_INDENT);
+      pw.print("#");
+      pw.print(settingName);
+      pw.print(" = ");
+      pw.println(value.render(ConfigRenderOptions.concise()));
+      pw.println();
+    }
+    pw.println(LINE_INDENT + "}");
+    pw.println("");
+    pw.println(LINE_INDENT + "advanced {");
+    pw.println("");
+    driverConfig =
+        LoaderConfigFactory.standaloneDSBulkReference().getConfig("datastax-java-driver.advanced");
+    for (Entry<String, ConfigValue> groupEntry : driverConfig.entrySet()) {
+      String settingName = groupEntry.getKey();
+      ConfigValue value = ConfigUtils.getNullSafeValue(driverConfig, settingName);
+      value.origin().comments().stream()
+          .filter(line -> !ConfigUtils.isTypeHint(line))
+          .filter(line -> !ConfigUtils.isLeaf(line))
+          .forEach(
+              l -> {
+                pw.print(LINE_INDENT + LINE_INDENT + "# ");
+                pw.println(wrapIndentedLines(l, 2));
+              });
+      pw.print(LINE_INDENT + LINE_INDENT + "# Type: ");
+      pw.println(ConfigUtils.getTypeString(driverConfig, settingName).orElse("arg"));
+      pw.print(LINE_INDENT + LINE_INDENT + "# Default value: ");
+      pw.println(value.render(ConfigRenderOptions.concise()));
+      pw.print(LINE_INDENT + LINE_INDENT);
+      pw.print("#");
+      pw.print(settingName);
+      pw.print(" = ");
+      pw.println(value.render(ConfigRenderOptions.concise()));
+      pw.println();
+    }
+    pw.println("}");
+  }
+
+  private static void printDSBulkConfiguration(PrintWriter pw) {
+    pw.println("dsbulk {");
+    pw.println("");
+
+    Config referenceConfig = LoaderConfigFactory.createReferenceConfig();
+    Map<String, SettingsGroup> groups = SettingsGroupFactory.createGroups(referenceConfig);
+
+    for (Map.Entry<String, SettingsGroup> groupEntry : groups.entrySet()) {
+      String section = groupEntry.getKey();
+      if (section.equals("Common")) {
+        // In this context, we don't care about the "Common" pseudo-section.
+        continue;
+      }
+      pw.println(INDENTED_ROW_OF_HASHES);
+      referenceConfig.getConfig(section).root().origin().comments().stream()
+          .filter(line -> !ConfigUtils.isTypeHint(line))
+          .filter(line -> !ConfigUtils.isLeaf(line))
+          .forEach(
+              l -> {
+                pw.print(LINE_INDENT + "# ");
+                pw.println(wrapIndentedLines(l, 1));
+              });
+      pw.println(INDENTED_ROW_OF_HASHES);
+
+      for (String settingName : groupEntry.getValue().getSettings()) {
+        ConfigValue value = ConfigUtils.getNullSafeValue(referenceConfig, settingName);
+
+        pw.println();
+        value.origin().comments().stream()
+            .filter(line -> !ConfigUtils.isTypeHint(line))
+            .filter(line -> !ConfigUtils.isLeaf(line))
+            .forEach(
+                l -> {
+                  pw.print(LINE_INDENT + "# ");
+                  pw.println(wrapIndentedLines(l, 1));
+                });
+        pw.print(LINE_INDENT + "# Type: ");
+        pw.println(ConfigUtils.getTypeString(referenceConfig, settingName).orElse("arg"));
+        pw.print(LINE_INDENT + "# Default value: ");
+        pw.println(value.render(ConfigRenderOptions.concise()));
+        pw.print(LINE_INDENT);
+        pw.print("#");
+        pw.print(settingName.substring("dsbulk.".length()));
+        pw.print(" = ");
+        pw.println(value.render(ConfigRenderOptions.concise()));
+      }
+      pw.println();
+    }
+    pw.println("}");
+  }
+
   @SuppressWarnings("SameParameterValue")
   private static String wrapLines(String text) {
     return WordUtils.wrap(text, LINE_LENGTH - 2, String.format("%n# "), false);
   }
 
-  private static String wrapIndentedLines(String text) {
+  private static String wrapIndentedLines(String text, int indentation) {
     return WordUtils.wrap(
-        text, LINE_LENGTH - INDENT_LENGTH - 2, String.format("%n%s# ", LINE_INDENT), false);
+        text,
+        (LINE_LENGTH - INDENT_LENGTH * indentation) - 2,
+        String.format("%n%s# ", StringUtils.nCopies(" ", INDENT_LENGTH * indentation)),
+        false);
   }
 }
