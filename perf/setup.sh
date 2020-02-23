9de8f5fcdb22d6401f4306291b69a2928e7ad7b9
Alexandre Dutra <alexandre.dutra@datastax.com>
2020-02-23 16:39:16 +0100

    Simplify performance test script

--- perf/setup.sh
+++ perf/setup.sh
@@ -32,7 +32,7 @@ ctool start dsbulk-dse opscenter
 ctool install_agents dsbulk-dse dsbulk-dse
 
 dse_ip=`ctool info --public-ips dsbulk-dse -n 0`
-curl "http://${dse_ip}:8888/opscenter/index.html" #verifying that ops-center started
+# Opscenter is accessible at http://${dse_ip}:8888/opscenter/index.html
 
 #setup data-set (random Partition Key)
 ctool run --sudo dsbulk-client "mkdir /mnt/data; chmod 777 /mnt/data"
@@ -46,8 +46,7 @@ ctool run --sudo dsbulk-client "cd /mnt/data/DSEBulkLoadTest/in/data100B; cat da
 ctool run --sudo dsbulk-client "sudo apt update --assume-yes; sudo apt install maven --assume-yes; sudo apt-get install unzip --assume-yes"
 
 #setup data-set (multiple records per Partition Key)
-github_username="username"; github_password="password";
-ctool run --sudo dsbulk-client "cd /mnt/data; sudo su automaton; git clone https://${github_username}:${github_password}@github.com/riptano/data_faker.git; cd data_faker; mvn clean package"
+ctool run --sudo dsbulk-client "cd /mnt/data; sudo su automaton; git clone https://github.com/riptano/data_faker.git; cd data_faker; mvn clean package"
 #generate 1 million PKs. Every PK has >= 50 && <= 100 records.
 ctool run --sudo dsbulk-client "cd /mnt/data/data_faker; java -jar target/fake-data-generator-1.0.jar 32 1000000 50 100 false"
 
@@ -72,21 +71,19 @@ ctool run dsbulk-dse 0 "cqlsh -e \"CREATE TABLE IF NOT EXISTS test.transactions(
 # TODO tweak settings.xml
 
 #to build dsbulk on dsbulk-client (dsbulk should not have SNAPSHOT dependencies to build on ctool created instance)
-#github_username="username"
-#github_password="password"
-#ctool run --sudo dsbulk-client "cd /mnt/data; git clone https://${github_username}:${github_password}@github.com/riptano/dsbulk.git"
+#ctool run --sudo dsbulk-client "cd /mnt/data; git clone https://github.com/riptano/dsbulk.git"
 #ctool run --sudo dsbulk-client "cd /mnt/data/dsbulk; sudo mvn clean package -DskipTests -P release"
 
 #to build locally and scp to dsbulk-client, you can change --branch parameter to test against different branch
-dsbulk_version=1.3.1-SNAPSHOT
 rm -rf /tmp/dsbulk
 mkdir /tmp/dsbulk
-cd /tmp/dsbulk
-`github_username="username"; github_password="password"; git clone --single-branch --branch 1.x https://${github_username}:${github_password}@github.com/riptano/dsbulk.git`
-cd dsbulk
+cd /tmp/dsbulk || exit
+git clone --single-branch --branch 1.x https://github.com/riptano/dsbulk.git
+cd dsbulk || exit
 mvn clean package -DskipTests -P release
-ctool scp -R dsbulk-client 0 /tmp/dsbulk/dsbulk/dist/target/*.zip /mnt/data/
-ctool run --sudo dsbulk-client "cd /mnt/data/; unzip *.zip; mv dsbulk-${dsbulk_version} dsbulk"
+ctool run --sudo dsbulk-client "cd /mnt/data/; rm -rf /mnt/data/dsbulk*"
+ctool scp -R dsbulk-client 0 dist/target/*.zip /mnt/data/
+ctool run --sudo dsbulk-client "cd /mnt/data/; unzip *.zip; rm *.zip; mv dsbulk-* dsbulk"
 
 # TODO single file vs multiple files (> # cores)
 
