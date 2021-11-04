a47291551307ff78ae5bfdc7d21785f16ba96c13
Sergey Matvienko <smatvienko@thingsboard.io>
2021-11-04 11:00:30 +0200

    Count workflow: Print results even if any failures. It has no sense to query billions rows for a few hours and show nothing if some failure happens

--- workflow/count/src/main/java/com/datastax/oss/dsbulk/workflow/count/CountWorkflow.java
+++ workflow/count/src/main/java/com/datastax/oss/dsbulk/workflow/count/CountWorkflow.java
@@ -207,10 +207,12 @@ public class CountWorkflow implements Workflow {
       if (metricsManager != null) {
         metricsManager.reportFinalMetrics();
       }
-      // only print totals if the operation was successful
-      if (success) {
-        readResultCounter.reportTotals();
-      }
+
+      // Print results even if any failures.
+      // It has no sense to query billions rows for a few hours and show nothing if some failure
+      // happens
+      readResultCounter.reportTotals();
+
       LOGGER.debug("{} closed.", this);
       if (e != null) {
         throw e;
