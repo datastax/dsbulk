0c91fdc4aa3e867decf4f080353b3bba468ce911
Alexandre Dutra <adutra@users.noreply.github.com>
2022-01-19 11:04:26 -0300

    Disable EndToEndCloudIT
    EndToEndCloudIT is failing because the SNI proxy
    image is not available anymore from DataStax
    internal CI infrastructure.

--- runner/src/it/java/com/datastax/oss/dsbulk/runner/cloud/EndToEndCloudIT.java
+++ runner/src/it/java/com/datastax/oss/dsbulk/runner/cloud/EndToEndCloudIT.java
@@ -59,6 +59,7 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -73,6 +74,7 @@ import ru.lanwen.wiremock.ext.WiremockResolver.Wiremock;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("long")
 @LogConfigurationResource("logback.xml")
+@Disabled(value = "SNI Proxy image not available anymore in CI")
 class EndToEndCloudIT {
 
   private final SNIProxyServer proxy;
